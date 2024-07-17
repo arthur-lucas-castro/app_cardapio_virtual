f87b24f87619c14a9f070001ecc437aa78c9d6fc
Arthur Castro <arthurcastro1117@gmail.com>
2024-07-16 21:57:27 -0300

    Criacao das exibicoes dos dados na tela de Admin

--- cardapio-virtual-admin/core/models.py
+++ cardapio-virtual-admin/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Cliente(models.Model):
     nome=models.CharField(max_length=30)
@@ -16,8 +17,8 @@ class Produto(models.Model):
     nome=models.CharField(max_length=30)
     preco= models.DecimalField(decimal_places=2, max_digits=5)
     descricao=models.TextField()
-    categoria=models.ForeignKey(Categoria, on_delete=models.DO_NOTHING, default=1)
-    imagem = models.ImageField(null=True, blank=True)
+    categoria=models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    imagem = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
@@ -29,16 +30,20 @@ class Pedido(models.Model):
     ativo = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
-        return str(self.mesa)
+        return str(self.id)
 
 class PedidoProduto(models.Model):
+    class StatusPedidos(models.TextChoices):
+        PEDIDO_REALIZADO = '1', _('Pedido Realizado')
+        PEDIDO_EM_PREPARO = '2', _('Pedido em Preparo')
+        PEDIDO_PRONTO = '3', _('Pedido Pronto')
+        PEDIDO_ENTREGUE = '4', _('Pedido Entregue')
     produtoId = models.ForeignKey(Produto, on_delete=models.DO_NOTHING, null=True)
     pedidoId = models.ForeignKey(Pedido, on_delete=models.DO_NOTHING, null=True)
-    mesa=models.IntegerField()
-    ativo = models.BooleanField(default=True)
+    status = models.CharField(max_length=1,choices=StatusPedidos.choices, default=StatusPedidos.PEDIDO_REALIZADO)
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
-        return str(self.mesa)
+        return str(self.id)
 
 
 
